e09cd5abb42f374a823810abfc2008f8c48d2360
niunaicoke <107735329+niunaicoke@users.noreply.github.com>
2025-05-13 10:03:03 +0800

    Update mean fixation time.py

--- mean fixation time.py
+++ mean fixation time.py
@@ -6,7 +6,9 @@ T= Time()
 
 
 
-def mean_fixations (t_before_task_in_seconds =120, t_after_task_in_seconds=120, minute=1, sec=1, label="x", data=pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")):
+def mean_fixations (minute=1, sec=1, label="x", data=pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")):
+    t_before_task_in_seconds =120
+    t_after_task_in_seconds=120
     start_timestamp = data.iloc[0]['start timestamp [ns]']+ (minute * 60 + sec) * 1e9
     pretask_ns = start_timestamp - t_before_task_in_seconds * 1e9
     posttask_ns = start_timestamp+ t_after_task_in_seconds * 1e9
@@ -18,5 +20,5 @@ def mean_fixations (t_before_task_in_seconds =120, t_after_task_in_seconds=120, 
     print(f"The mean fixations for {label} is {duration / num_fixations} ms per fixation")
 
 data = pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")
-x=mean_fixations(60,60, T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", data)
+x=mean_fixations(T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", data)
 
