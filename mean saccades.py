628ab04030357d9fdfe418e6edcbde74fae325e0
niunaicoke <107735329+niunaicoke@users.noreply.github.com>
2025-05-13 10:05:20 +0800

    Update mean saccades.py

--- mean saccades.py
+++ mean saccades.py
@@ -5,6 +5,8 @@ T= Time()
 
 
 def mean_saccades (t_before_task_in_seconds =120, t_after_task_in_seconds=120, minute=1, sec=1, label="x", data=pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")):
+    t_before_task_in_seconds =120
+    t_after_task_in_seconds=120
     start_timestamp = data.iloc[0]['start timestamp [ns]']+ (minute * 60 + sec) * 1e9
     pretask_ns = start_timestamp - t_before_task_in_seconds * 1e9
     posttask_ns = start_timestamp+ t_after_task_in_seconds * 1e9
@@ -14,6 +16,6 @@ def mean_saccades (t_before_task_in_seconds =120, t_after_task_in_seconds=120, m
     print(f"The mean saccades for {label} is {int(num_saccades) / interval} per second")
 
 data = pd.read_csv("/home/yulab/Desktop/N1/N1_P1/saccades.csv")
-x=mean_saccades(60,60, T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", data)
+x=mean_saccades(T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", data)
 
 
