2affc46275c7de0674eb4b99b81bf224fa7917ca
niunaicoke <107735329+niunaicoke@users.noreply.github.com>
2025-05-13 10:04:44 +0800

    Update mean saccades time.py

--- mean saccades time.py
+++ mean saccades time.py
@@ -6,7 +6,9 @@ T= Time()
 
 
 
-def mean_saccades (t_before_task_in_seconds =120, t_after_task_in_seconds=120, minute=1, sec=1, label="x", data=pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")):
+def mean_saccades (minute=1, sec=1, label="x", data=pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")):
+    t_before_task_in_seconds =120
+    t_after_task_in_seconds=120
     start_timestamp = data.iloc[0]['start timestamp [ns]']+ (minute * 60 + sec) * 1e9
     pretask_ns = start_timestamp - t_before_task_in_seconds * 1e9
     posttask_ns = start_timestamp+ t_after_task_in_seconds * 1e9
@@ -18,5 +20,5 @@ def mean_saccades (t_before_task_in_seconds =120, t_after_task_in_seconds=120, m
     print(f"The mean saccades for {label} is {duration / num_saccades} ms per saccade")
 
 saccades = pd.read_csv("/home/yulab/Desktop/N1/N1_P1/saccades.csv")
-mean_saccades(60,60, T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", saccades)
+mean_saccades(T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", saccades)
 
