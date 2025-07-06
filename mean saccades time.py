57cfe62317a307419bf18cf5ee08efb3aef9419e
niunaicoke <107735329+niunaicoke@users.noreply.github.com>
2025-07-06 21:20:17 +0800

    Add files via upload

--- mean saccades time.py
+++ mean saccades time.py
@@ -6,19 +6,32 @@ T= Time()
 
 
 
-def mean_saccades (minute=1, sec=1, label="x", data=pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")):
+def mean_saccades (minute=1, sec=1, label="x", data=""):
     t_before_task_in_seconds =120
     t_after_task_in_seconds=120
     start_timestamp = data.iloc[0]['start timestamp [ns]']+ (minute * 60 + sec) * 1e9
     pretask_ns = start_timestamp - t_before_task_in_seconds * 1e9
     posttask_ns = start_timestamp+ t_after_task_in_seconds * 1e9
-    interval = t_before_task_in_seconds+ t_after_task_in_seconds
     df = data[(data['start timestamp [ns]'] >= pretask_ns) & (data['end timestamp [ns]'] <= posttask_ns)]
     duration = pd.Series(df['duration [ms]'])
     duration = duration.sum()
     num_saccades = df["saccade id"].count()
     print(f"The mean saccades for {label} is {duration / num_saccades} ms per saccade")
 
-saccades = pd.read_csv("/home/yulab/Desktop/N1/N1_P1/saccades.csv")
-mean_saccades(T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", saccades)
 
+
+
+
+
+for i in range(1, 10):
+    # P1
+    data = pd.read_csv(f"/Users/scottliu/Desktop/Peiran Processed Data/N{i}/N{i}_P1/saccades.csv")
+    x = mean_saccades(getattr(T, f"N{i}_P1_e")[0], getattr(T, f"N{i}_P1_e")[1], f"N{i}_P1_e", data)
+    x = mean_saccades(getattr(T, f"N{i}_P1_m")[0], getattr(T, f"N{i}_P1_m")[1], f"N{i}_P1_m", data)
+    x = mean_saccades(getattr(T, f"N{i}_P1_h")[0], getattr(T, f"N{i}_P1_h")[1], f"N{i}_P1_h", data)
+
+    # P2
+    data = pd.read_csv(f"/Users/scottliu/Desktop/Peiran Processed Data/N{i}/N{i}_P2/saccades.csv")
+    x = mean_saccades(getattr(T, f"N{i}_P2_e")[0], getattr(T, f"N{i}_P2_e")[1], f"N{i}_P2_e", data)
+    x = mean_saccades(getattr(T, f"N{i}_P2_m")[0], getattr(T, f"N{i}_P2_m")[1], f"N{i}_P2_m", data)
+    x = mean_saccades(getattr(T, f"N{i}_P2_h")[0], getattr(T, f"N{i}_P2_h")[1], f"N{i}_P2_h", data)
--- mean saccades.py
+++ mean saccades.py
@@ -4,7 +4,7 @@ T= Time()
 
 
 
-def mean_saccades (t_before_task_in_seconds =120, t_after_task_in_seconds=120, minute=1, sec=1, label="x", data=pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")):
+def mean_saccades (minute=1, sec=1, label="x", data=""):
     t_before_task_in_seconds =120
     t_after_task_in_seconds=120
     start_timestamp = data.iloc[0]['start timestamp [ns]']+ (minute * 60 + sec) * 1e9
@@ -15,7 +15,18 @@ def mean_saccades (t_before_task_in_seconds =120, t_after_task_in_seconds=120, m
     num_saccades = df["saccade id"].count()
     print(f"The mean saccades for {label} is {int(num_saccades) / interval} per second")
 
-data = pd.read_csv("/home/yulab/Desktop/N1/N1_P1/saccades.csv")
-x=mean_saccades(T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", data)
+
+for i in range(1, 10):
+    # P1
+    data = pd.read_csv(f"/Users/scottliu/Desktop/Peiran Processed Data/N{i}/N{i}_P1/saccades.csv")
+    x = mean_saccades(getattr(T, f"N{i}_P1_e")[0], getattr(T, f"N{i}_P1_e")[1], f"N{i}_P1_e", data)
+    x = mean_saccades(getattr(T, f"N{i}_P1_m")[0], getattr(T, f"N{i}_P1_m")[1], f"N{i}_P1_m", data)
+    x = mean_saccades(getattr(T, f"N{i}_P1_h")[0], getattr(T, f"N{i}_P1_h")[1], f"N{i}_P1_h", data)
+
+    # P2
+    data = pd.read_csv(f"/Users/scottliu/Desktop/Peiran Processed Data/N{i}/N{i}_P2/saccades.csv")
+    x = mean_saccades(getattr(T, f"N{i}_P2_e")[0], getattr(T, f"N{i}_P2_e")[1], f"N{i}_P2_e", data)
+    x = mean_saccades(getattr(T, f"N{i}_P2_m")[0], getattr(T, f"N{i}_P2_m")[1], f"N{i}_P2_m", data)
+    x = mean_saccades(getattr(T, f"N{i}_P2_h")[0], getattr(T, f"N{i}_P2_h")[1], f"N{i}_P2_h", data)
 
 
