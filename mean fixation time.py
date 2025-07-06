efd4c6a135c7db2ab7ab4c59a175e227753427cb
niunaicoke <107735329+niunaicoke@users.noreply.github.com>
2025-07-06 21:12:15 +0800

    Add files via upload

--- mean fixation time.py
+++ mean fixation time.py
@@ -6,19 +6,31 @@ T= Time()
 
 
 
-def mean_fixations (minute=1, sec=1, label="x", data=pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")):
+def mean_fixations (minute=1, sec=1, label="x", data=""):
     t_before_task_in_seconds =120
     t_after_task_in_seconds=120
     start_timestamp = data.iloc[0]['start timestamp [ns]']+ (minute * 60 + sec) * 1e9
     pretask_ns = start_timestamp - t_before_task_in_seconds * 1e9
     posttask_ns = start_timestamp+ t_after_task_in_seconds * 1e9
-    interval = t_before_task_in_seconds+ t_after_task_in_seconds
     df = data[(data['start timestamp [ns]'] >= pretask_ns) & (data['end timestamp [ns]'] <= posttask_ns)]
     duration = pd.Series(df['duration [ms]'])
     duration = duration.sum()
     num_fixations = df["fixation id"].count()
     print(f"The mean fixations for {label} is {duration / num_fixations} ms per fixation")
 
-data = pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")
-x=mean_fixations(T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", data)
 
+
+
+
+for i in range(1, 10):
+    # P1
+    data = pd.read_csv(f"/Users/scottliu/Desktop/Peiran Processed Data/N{i}/N{i}_P1/fixations.csv")
+    x = mean_fixations(getattr(T, f"N{i}_P1_e")[0], getattr(T, f"N{i}_P1_e")[1], f"N{i}_P1_e", data)
+    x = mean_fixations(getattr(T, f"N{i}_P1_m")[0], getattr(T, f"N{i}_P1_m")[1], f"N{i}_P1_m", data)
+    x = mean_fixations(getattr(T, f"N{i}_P1_h")[0], getattr(T, f"N{i}_P1_h")[1], f"N{i}_P1_h", data)
+
+    # P2
+    data = pd.read_csv(f"/Users/scottliu/Desktop/Peiran Processed Data/N{i}/N{i}_P2/fixations.csv")
+    x = mean_fixations(getattr(T, f"N{i}_P2_e")[0], getattr(T, f"N{i}_P2_e")[1], f"N{i}_P2_e", data)
+    x = mean_fixations(getattr(T, f"N{i}_P2_m")[0], getattr(T, f"N{i}_P2_m")[1], f"N{i}_P2_m", data)
+    x = mean_fixations(getattr(T, f"N{i}_P2_h")[0], getattr(T, f"N{i}_P2_h")[1], f"N{i}_P2_h", data)
--- mean fixations.py
+++ mean fixations.py
@@ -4,18 +4,68 @@ T= Time()
 
 
 
-def mean_fixations (minute=1, sec=1, label="x", data=pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")):
+def mean_fixations (minute=1, sec=1, label="x",data_path=""):
     t_before_task_in_seconds =120
     t_after_task_in_seconds=120
-    start_timestamp = data.iloc[0]['start timestamp [ns]']+ (minute * 60 + sec) * 1e9
+    start_timestamp = data_path.iloc[0]['start timestamp [ns]']+ (minute * 60 + sec) * 1e9
     pretask_ns = start_timestamp - t_before_task_in_seconds * 1e9
     posttask_ns = start_timestamp+ t_after_task_in_seconds * 1e9
     interval = t_before_task_in_seconds+ t_after_task_in_seconds
-    df = data[(data['start timestamp [ns]'] >= pretask_ns) & (data['end timestamp [ns]'] <= posttask_ns)]
+    df = data_path[(data_path['start timestamp [ns]'] >= pretask_ns) & (data_path['end timestamp [ns]'] <= posttask_ns)]
     num_fixations = df["fixation id"].count()
     print(f"The mean fixations for {label} is {int(num_fixations) / interval} per second")
 
-data = pd.read_csv("/home/yulab/Desktop/N1/N1_P1/fixations.csv")
-x=mean_fixations(T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", data)
 
 
+# #N1
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N1/N1_P1/fixations.csv")
+# x=mean_fixations(T.N1_P1_e[0], T.N1_P1_e[1], "N1_P1_e", data)
+#
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N1/N1_P1/fixations.csv")
+# x=mean_fixations(T.N1_P1_m[0], T.N1_P1_m[1], "N1_P1_m", data)
+#
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N1/N1_P1/fixations.csv")
+# x=mean_fixations(T.N1_P1_h[0], T.N1_P1_h[1], "N1_P1_h", data)
+#
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N1/N1_P2/fixations.csv")
+# x = mean_fixations(T.N1_P2_e[0], T.N1_P2_e[1], "N1_P2_e", data)
+#
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N1/N1_P2/fixations.csv")
+# x = mean_fixations(T.N1_P2_m[0], T.N1_P2_m[1], "N1_P2_m", data)
+#
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N1/N1_P2/fixations.csv")
+# x = mean_fixations(T.N1_P2_h[0], T.N1_P2_h[1], "N1_P2_h", data)
+#
+#
+# #N2
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N2/N2_P1/fixations.csv")
+# x=mean_fixations(T.N2_P1_e[0], T.N2_P1_e[1], "N2_P1_e", data)
+#
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N2/N2_P1/fixations.csv")
+# x=mean_fixations(T.N2_P1_m[0], T.N2_P1_m[1], "N2_P1_m", data)
+#
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N2/N2_P1/fixations.csv")
+# x=mean_fixations(T.N2_P1_h[0], T.N2_P1_h[1], "N2_P1_h", data)
+#
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N2/N2_P2/fixations.csv")
+# x = mean_fixations(T.N2_P2_e[0], T.N2_P2_e[1], "N2_P2_e", data)
+#
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N2/N2_P2/fixations.csv")
+# x = mean_fixations(T.N2_P2_m[0], T.N2_P2_m[1], "N2_P2_m", data)
+#
+# data = pd.read_csv("/Users/scottliu/Desktop/Peiran Processed Data/N2/N2_P2/fixations.csv")
+# x = mean_fixations(T.N2_P2_h[0], T.N2_P2_h[1], "N2_P2_h", data)
+
+
+for i in range(1, 10):
+    # P1
+    data = pd.read_csv(f"/Users/scottliu/Desktop/Peiran Processed Data/N{i}/N{i}_P1/fixations.csv")
+    x = mean_fixations(getattr(T, f"N{i}_P1_e")[0], getattr(T, f"N{i}_P1_e")[1], f"N{i}_P1_e", data)
+    x = mean_fixations(getattr(T, f"N{i}_P1_m")[0], getattr(T, f"N{i}_P1_m")[1], f"N{i}_P1_m", data)
+    x = mean_fixations(getattr(T, f"N{i}_P1_h")[0], getattr(T, f"N{i}_P1_h")[1], f"N{i}_P1_h", data)
+
+    # P2
+    data = pd.read_csv(f"/Users/scottliu/Desktop/Peiran Processed Data/N{i}/N{i}_P2/fixations.csv")
+    x = mean_fixations(getattr(T, f"N{i}_P2_e")[0], getattr(T, f"N{i}_P2_e")[1], f"N{i}_P2_e", data)
+    x = mean_fixations(getattr(T, f"N{i}_P2_m")[0], getattr(T, f"N{i}_P2_m")[1], f"N{i}_P2_m", data)
+    x = mean_fixations(getattr(T, f"N{i}_P2_h")[0], getattr(T, f"N{i}_P2_h")[1], f"N{i}_P2_h", data)
